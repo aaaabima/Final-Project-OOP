43cded4ee2f9d3fdccec8e93e2a9936043b8e186
Syakira Rahma <syakirarahma7@gmail.com>
2020-12-01 10:26:21 +0700

    Sprint 1 Report, Updated Version, Sprint 2

--- src/ClickListener.java
+++ src/ClickListener.java
@@ -1,7 +1,7 @@
 /*
 | NPM           | Name                            |
 | ------------- |-------------------------------- |
-| 140810190013  | Syakira Rahma Fauziah           |
+| 140810190013  | Syakira Rahma Fauziyah          |
 | 140810190021  | Mochammad Ghifari Eka Narayana  |
 | 140810190031  | Mochamad Arya Bima Agfian       |
 | 140810190051  | Salma Tri Audryani              |
@@ -9,62 +9,89 @@
 
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
-import javax.swing.JFrame;
 
 public class ClickListener extends MouseAdapter{
-    private int tileSize; // ukuran || dimensi tiles di UI
-    private int[] tiles; // tiles itu sendiri yang memiliki nilai index unique
-    private int blankPos;   // posisi tiles kosong
-    private int size;   // ukuran board game yang dimainkan
-    private int gridSize;   // ukuran grid UI
-    private int margin; // margin grid dari frame
-    private JFrame frame;
+    private int tileSize;       //ukuran || dimensi tiles di UI
+    private int[] tiles;        //tiles itu sendiri yang memiliki nilai index unique
+    private int blankPos;       //posisi tiles kosong
+    private int size;           //ukuran board game yang dimainkan
+    private int gridSize;       //ukuran grid UI
+    private int margin;         //margin grid dari fram
+    private int dimension;      //dimension grid dari frame
+    private boolean gameOver;   //gameover
+    private PuzzleGrid pg;      //objek PuzzleGrid
+    private Logic logic;        //objek Logic
 
-    public ClickListener(){
-        System.out.println("CilckListener Implemented.");
+    public ClickListener(PuzzleGrid pg){
+        this.pg = pg;
+        this.logic = new Logic();
+        this.size = pg.getGridSize();
+        this.margin = pg.getMargin();
+        this.dimension = pg.getDimension();
+
+        //menghitung jumlah tile
+        this.tiles = pg.getTiles();
+
+        //menghitung ukuran grid dan tile puzzle
+        this.gridSize = (dimension - 2 * margin);
+        this.tileSize = gridSize / size;
+
+        this.blankPos = tiles.length - 1;
     }
-    
+
     @Override
     public void mousePressed(MouseEvent e){
-        //get position of the click
-        int ex = e.getX() - margin;
-        int ey = e.getY() - margin;
+        //menyesuaikan gameStatus di PuzzleGrid
+        gameOver = pg.getGameStatus();
+        if(gameOver){
+            pg.newGame();
+        }else{
+            //posisi ketika cursor melakakukan 'click'
+            int ex = e.getX() - margin;
+            int ey = e.getY() - margin;
 
-        System.out.println("x : " + ex + " & y : " + ey);
+            //jika di luar grid (bernilai negatif) atau melebihi ukuran grid
+            //maka tidak akan ada yang dijalankan
+            if(ex < 0 || ex > gridSize || ey < 0 || ey > gridSize)
+            return;
 
-        //click in the grid
-        if(ex < 0 || ex > gridSize || ey < 0 || ey > gridSize)
-        return;
+            //mengambil posisi grid
+            int c1 = ex / tileSize;
+            int r1 = ey / tileSize;
 
-        //get position in the grid
-        int c1 = ex / tileSize;
-        int r1 = ey / tileSize;
+            //mengambil posisi blankPos
+            int c2 = blankPos % size;
+            int r2 = blankPos / size;
 
-        //get position of the blank cell
-        int c2 = blankPos % size;
-        int r2 = blankPos / size;
+            int clickPos = r1 * size + c1;
 
+            //arah pergerakan
+            int dir = 0;
 
-        int clikPos = r1 * size + c1;
+            //membandingkan grid yang diklik dengan posisi blankPos
+            if(c1 == c2 && Math.abs(r1 - r2) > 0)
+            dir = (r1 - r2) > 0 ? size : -size;
+            else if(r1 == r2 && Math.abs(c1 - c2) > 0)
+            dir = (c1 - c2) > 0 ? 1 : -1;
 
-        int dir = 0;
+            if(dir != 0){
+                //memindahkan tiles
+                do{
+                    int newBlankPos = blankPos + dir;
+                    tiles[blankPos] = tiles[newBlankPos];
+                    blankPos = newBlankPos;
+                }
+                while(blankPos != clickPos);
+                tiles[blankPos] = 0;
+                }
 
-        
-        if(c1 == c2 && Math.abs(r1 - r2) > 0)
-        dir = (r1 - r2) > 0 ? size : -size;
-        else if(r1 == r2 && Math.abs(c1 - c2) > 0)
-        dir = (c1 - c2) > 0 ? 1 : -1;
-
-        if(dir != 0){
-            //we move tiles in the direction
-            do{
-                int newBlankPos = blankPos + dir;
-                tiles[blankPos] = tiles[newBlankPos];
-                blankPos = newBlankPos;
+                //jika semua tiles berada di tempat yang seharusnya, maka gameOver
+                if(login.isSolved(this.tiles)){
+                    pg.setGameOver();
+                    this.gameOver = true;
+                }
             }
-            while(blankPos != clikPos);
-            tiles[blankPos] = 0;
-        }
-        frame.repaint();
+        //paint ulang ketika grid mengalami perubahan di sini
+        pg.repaint();
     }
 }
